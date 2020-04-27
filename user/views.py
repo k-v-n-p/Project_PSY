fc3db4411f23e932ff21ac25325d9d3b32ccf229
k-v-n-p <54201355+k-v-n-p@users.noreply.github.com>
2020-04-27 12:04:59 +0530

    Update views.py

--- user/views.py
+++ user/views.py
@@ -1,31 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
-posts = [
-    {
-        'author':'KVNP',
-        'title':'1st post',
-        'content':'First post content',
-        'date_posted':'18 april,2020'
-    },
-    {
-        'author':'KVNP',
-        'title':'2nd post',
-        'content':'second post content',
-        'date_posted':'18 april,2020'
-    },
-
-]
-
 
 def room(request):
     context={
-    #'posts': posts
+    # control settings for our hardware
     }
     return render(request,'user/room.html',context)
 
 def billing(request):
     return render(request,'user/billing.html',{'title':'biling'})
+
+
 
